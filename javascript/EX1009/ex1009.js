c3f43c5a6f3cd120b80cdfcaaa1665c3e64fa9b9
Júlio Mazotti <juliomazotti@hotmail.com>
2023-10-27 13:56:13 -0300

    chore: remove unnecessary comments

--- javascript/EX1009/ex1009.js
+++ javascript/EX1009/ex1009.js
@@ -3,19 +3,6 @@ var lines = input.split('\n');
 
 // Expected output: TOTAL = R$ 1884.58
 
-// const checarSePrimeiroEhNome = lines.join("").match(/[a-z]/i);
-// const checarSeSegundoEhNumero = lines.join("").match(/\d/);
-// const checarSeTerceiroEhNumero = lines.join("").match(/\d/);
-
-// if (
-//     !checarSePrimeiroEhNome && 
-//     !checarSeSegundoEhNumero &&
-//     !checarSeTerceiroEhNumero
-//     ) {
-//     console.log("Inputs inválidos.")
-//     return;
-// }
-
 const name = lines[0];
 const salary = +lines[1];
 const sales = +lines[2];
